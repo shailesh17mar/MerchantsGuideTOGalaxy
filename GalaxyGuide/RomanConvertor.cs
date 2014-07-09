5357f272850f65463aa80e95715e3d82a076b5d0
unknown <shailesh17mar@gmail.com>
2014-07-09 18:55:19 +0530

    Project added to GIT

---
+++ GalaxyGuide/RomanConvertor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GalaxyGuide
+{
+    public class RomanConvertor
+    {
+        private List<RomanChart> _romanString;
+
+        private RomanNumberValidationChainManager _validationChainManager;
+
+        private DLVRepetitionValidator _validationChainHeader;
+
+        public RomanConvertor()
+        {
+            _romanString=new List<RomanChart>();
+            _validationChainHeader=new DLVRepetitionValidator();
+            _validationChainManager = new RomanNumberValidationChainManager(_validationChainHeader);
+            _validationChainManager.Setup();
+        }
+
+        public int Convert(string romanNumberString)
+        {
+            try
+            {
+                StringToEnumArray(romanNumberString);
+                _validationChainHeader.Validate(_romanString);
+
+                return CalculateValue();
+            }
+            catch (ArgumentException e)
+            {
+                throw;
+            }
+
+        }
+
+        private void StringToEnumArray(string romanNumberString)
+        {
+            foreach (var c in romanNumberString)
+            {
+                _romanString.Add((RomanChart)Enum.Parse(typeof(RomanChart), c.ToString(CultureInfo.InvariantCulture), true));
+            }
+        }
+
+        private int CalculateValue()
+        {
+            var sum = 0;
+            for (var i = 0; i < _romanString.Count ; i++)
+            {
+                if (i < _romanString.Count-1)
+                {
+                    if ((int)_romanString[i] < (int)_romanString[i + 1])
+                    {
+                        sum = sum + (int)_romanString[i + 1] - (int)_romanString[i];
+                        i++;
+                        continue;
+                    }
+                }
+                
+                sum += (int)_romanString[i];
+            }
+            return sum;
+        }
+
+    }
+
+
+    public enum RomanChart
+    {
+        I = 1,
+        V = 5,
+        X = 10,
+        L = 50,
+        C = 100,
+        D = 500,
+        M = 1000
+    }
+}
---
+++ GalaxyGuideTests/RomanConvertorTests.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaxyGuide;
+using NUnit.Framework;
+
+namespace GalaxyGuideTests
+{
+    public class RomanConvertorTests
+    {
+        private RomanConvertor _convertor;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _convertor = new RomanConvertor();
+        }   
+
+        [Test]
+        public void DoNothing()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void IShouldReturnOne()
+        {
+            var number = _convertor.Convert("I");
+            Assert.AreEqual(number, 1);
+        }
+
+        [Test]
+        public void IIShouldReturnTwo()
+        {
+            var number = _convertor.Convert("II");
+            Assert.AreEqual(2, number);
+        }
+
+        [Test]
+        public void IIIShouldReturnThree()
+        {
+            var number = _convertor.Convert("III");
+            Assert.AreEqual(3, number);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void IIIIShouldThrowInvalidNumberException()
+        {
+            var number = _convertor.Convert("IIII");
+        }
+
+        [Test]
+        public void VShouldReturnFive()
+        {
+            var number = _convertor.Convert("V");
+            Assert.AreEqual(5,number);
+        }
+
+        [Test]
+        public void VIShouldReturnSix()
+        {
+            var number = _convertor.Convert("VI");
+            Assert.AreEqual(6, number);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void VIIIIShouldThrowInvalidNumberException()
+        {
+            var number = _convertor.Convert("VIIII");
+        }
+
+        [Test]
+        public void IVShouldReturnFour()
+        {
+            var number = _convertor.Convert("IV");
+            Assert.AreEqual(4, number);
+        }
+
+        [Test]
+        public void IXShouldReturnNine()
+        {
+            var number = _convertor.Convert("IX");
+            Assert.AreEqual(9, number);
+        }
+
+        [Test]
+        public void XIXShouldReturnNineteen()
+        {
+            var number = _convertor.Convert("XIX");
+            Assert.AreEqual(19, number);
+        }
+
+        [Test]
+        public void XXIVShouldReturnTwentyFour()
+        {
+            var number = _convertor.Convert("XXIV");
+            Assert.AreEqual(24, number);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldNotContainMoreThanOneD()
+        {
+            var number = _convertor.Convert("DD");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldNotContainMoreThanOneL()
+        {
+            var number = _convertor.Convert("LL");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ShouldNotContainMoreThanOneV()
+        {
+            var number = _convertor.Convert("VIV");
+        }
+
+        [Test]
+        public void MCMIIIShouldReturn1903()
+        {
+            var number = _convertor.Convert("MCMIII");
+            Assert.AreEqual(1903, number);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ILShouldThrowException()
+        {
+            var number = _convertor.Convert("IL");
+        }
+
+
+    }
+}
